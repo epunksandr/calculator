e94c129089fd30beee409512cff110b87e86d142
oleksandr <oleksandr@epunks.de>
2025-07-18 12:50:29 +0200

    built frontend and basic operations in js

---
+++ script.js
@@ -0,0 +1,29 @@
+// let input = document.querySelector(".input");
+// const clearBtn = document.querySelector(".clear");
+// const btn1 = document.querySelector("#btn1");
+// const btn2 = document.querySelector("#btn2");
+// const btn3 = document.querySelector("#btn3");
+// const btn4 = document.querySelector("#btn4");
+
+// const buttons = document.querySelectorAll(".buttons");
+// let initialValue = 0;
+
+// one.addEventListener("click", () => input.textContent = 1)
+
+// clearBtn.addEventListener("click", () => input.textContent = initialValue)
+
+function add (prev, current) {
+    return prev + current;
+}
+
+function subtract (prev, current) {
+    return prev - current;
+}
+
+function multiply(prev, current) {
+    return prev * current;
+}
+
+function divide(prev, current) {
+    return prev / current;
+}
